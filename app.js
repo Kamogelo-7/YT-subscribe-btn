7d9cb9113cb1cd70cded850e314188db3bc33b21
Kamogelo <ekmogasoa@gmail.com>
2025-02-02 17:26:09 +0200

    added fetch API for images in json

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ let subs = 0;
 
 const btn = document.querySelector(".btn");
 const subCount = document.querySelector(".sub-count");
+const imageFile = document.querySelector(".channel-name");
+// const randomizeImage = Math.floor(Math.random(imageFile));
+
+const imgData = fetch("imageFiles.json");
+const checkImages = async () => {
+  const res = await imgData;
+  const data = await res.json();
+  console.log(data[1]);
+};
+checkImages();
 
 subCount.textContent = `${subs} Subscribers`;
 
